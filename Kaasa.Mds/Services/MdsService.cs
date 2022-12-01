911370ad8f049ac2b19516e71393a9a11b8e1b06
Lennard Helmich <lennard.helmich@gmx.de>
2022-12-01 16:06:54 +0100

    Added Logger and Guards for MdsService

--- Kaasa.Mds/Services/MdsService.cs
+++ Kaasa.Mds/Services/MdsService.cs
@@ -1,7 +1,10 @@
-﻿namespace Kaasa.Mds.Services;
+﻿using Java.Util;
+
+namespace Kaasa.Mds.Services;
 
 public sealed partial class MdsService : IMdsService
 {
+    private readonly ILogger<MdsService> _logger;
     internal List<MdsDevice> MdsDevices { get; } = new();
 
     public event EventHandler<string>? OnConnect;
@@ -14,7 +17,11 @@ public sealed partial class MdsService : IMdsService
 
     public async Task<IMdsDevice> ConnectAsync(Guid uuid)
     {
+        Guard.IsNotNull(uuid, nameof(uuid));
+
         var device = MdsDevices.FirstOrDefault(x => x.UUID == uuid);
+
+        _logger.LogDebug("Trying to connect device {0}.", uuid);
 
         if (device != null)
             return device;
@@ -24,16 +31,25 @@ public sealed partial class MdsService : IMdsService
 
     public IMdsDevice? GetConnectedSensor(Guid uuid)
     {
+        Guard.IsNotNull(uuid, nameof(uuid));
+
         return MdsDevices.FirstOrDefault(x => x.UUID == uuid);
     }
 
     public IMdsDevice? GetConnectedSensor(string serial)
     {
+        Guard.IsNotNullOrWhiteSpace(serial, nameof(serial));
+
         return MdsDevices.FirstOrDefault(x => x.Serial == serial);
     }
 
     private void MdsServiceOnConnectionComplete(object? sender, (Guid uuid, string serial) e)
     {
+        Guard.IsNotNull(e.uuid, nameof(e.uuid));
+        Guard.IsNotNullOrWhiteSpace(e.serial, nameof(e.serial));
+
+        _logger.LogDebug("Trying to establish subscriptions for device {0}.", e.uuid);
+
         var device = MdsDevices.FirstOrDefault(x => x.Serial == e.serial);
 
         if (device is null)
